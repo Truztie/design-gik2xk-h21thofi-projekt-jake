d03330907a406e2b3215223782bb35743722e7b3
Thomas Finneman <thomasfinneman94@gmail.com>
2023-03-07 19:00:16 +0100

    backend

--- server/models/shoppingCartRow.js
+++ server/models/shoppingCartRow.js
@@ -3,13 +3,7 @@ const sequelize = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define(
     "shoppingCartRow",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      }
-    }, 
+    {}, 
     { underscored: true }
   );
 };
